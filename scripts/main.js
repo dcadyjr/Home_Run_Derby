375fd7eca17fe3f6ea41688c2c8ceafd60a3b98f
David Cady Jr <davidcadyjr@mac.com>
2017-06-07 15:45:35 -0500

    draw ball function working

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,24 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+
+
+// ctx.beginPath();
+// 		ctx.arc(500, 300, 10, 0, Math.PI*2);
+// 		ctx.stroke();
+
+var ball = {
+	position: {x:500, y:300},
+	direction: "",
+	move: function(){
+ //stuff to pith the ball
+ 		ball.position.y++
+	},
+	draw: function(){
+		ctx.beginPath();
+		ctx.arc(ball.position.x, ball.position.y, 10, 0, Math.PI*2);
+		ctx.stroke();
+	},
+
+}
+	ball.draw();
+	ball.move();
