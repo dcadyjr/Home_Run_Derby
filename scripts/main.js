d3ef21f28d415d4a04fc7be23d2dece3f062d80f
David Cady Jr <davidcadyjr@mac.com>
2017-06-07 16:01:03 -0500

    ball is being pitched

--- scripts/main.js
+++ scripts/main.js
@@ -2,23 +2,33 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 
-// ctx.beginPath();
-// 		ctx.arc(500, 300, 10, 0, Math.PI*2);
-// 		ctx.stroke();
+
 
 var ball = {
 	position: {x:500, y:300},
 	direction: "",
 	move: function(){
- //stuff to pith the ball
+ //move the ball down the Y axis
  		ball.position.y++
+ 		console.log(ball.position.y);
 	},
 	draw: function(){
+		ctx.clearRect(0,0,canvas.width,canvas.height);
 		ctx.beginPath();
 		ctx.arc(ball.position.x, ball.position.y, 10, 0, Math.PI*2);
 		ctx.stroke();
+
 	},
 
 }
 	ball.draw();
 	ball.move();
+
+	var animateCanvas = function(){
+ 
+    ball.move();
+
+  	ball.draw();
+  	window.requestAnimationFrame(animateCanvas);
+}
+ animateCanvas();
