1c5a3f25795d5e4973c450b85b323067734d77db
David Cady Jr <davidcadyjr@mac.com>
2017-06-08 10:57:44 -0500

    made conditional for hit between  640 and 645 pixels. created homerun variable

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var randomNumber = Math.floor(Math.random() * ((-10 - 10)+ 1) + 10);
-
+var homeRun = 0;
 
 // var drawBackground = function() {
 // 	var img = new Image();
@@ -13,12 +13,12 @@ var randomNumber = Math.floor(Math.random() * ((-10 - 10)+ 1) + 10);
 // drawBackground ();
 
 var ball = {
-	position: {x:500, y:300},
+	position: {x:600, y:570},
 	direction: "pitch",
 	move: function(){
  //move the ball down the Y axis
  		if (ball.direction === "pitch"){
- 			ball.position.y += 5;
+ 			ball.position.y += 1;
  		} else if (ball.direction === "hit"){
  			ball.position.y -= 5;
  			ball.position.x += randomNumber;
@@ -32,13 +32,9 @@ var ball = {
 		ctx.arc(ball.position.x, ball.position.y, 3, 0, Math.PI*2);
 		ctx.fill();
 
-		
-
 	},
 
 }
-	ball.draw();
-	ball.move();
 
 var animateCanvas = function(){
  
@@ -48,14 +44,19 @@ var animateCanvas = function(){
   	window.requestAnimationFrame(animateCanvas);
 }
 animateCanvas();
-
+  // && ball.position.y >= 635
 document.addEventListener("keydown", function(){
  	var key = event.which;
- 		if (key === 32){
- 			ball.direction = "hit"
+ 		if (key === 72 && ball.position.y >= 640 && ball.position.y <= 645) {
+ 			console.log(ball.position.y);
+ 			ball.direction = "hit";
  			ball.draw();
 			ball.move();
- 		} else {
-
+			homeRun++
+			console.log(homeRun);
+ 		} else if (key === 72) {
+ 			
+ 		} else if (key === 72) {
+ 			console.log(ball.position.y);
  		}
  })
