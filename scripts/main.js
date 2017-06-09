1763a6fe6c18f94330c43312fc041cb1571fda98
David Cady Jr <davidcadyjr@mac.com>
2017-06-08 22:53:16 -0500

    added some commenting of my code

--- scripts/main.js
+++ scripts/main.js
@@ -1,78 +1,77 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-var randomNumber = Math.floor(Math.random() * ((-10 - 10)+ 1) + 10);
-var homeRun = 0;
-var totalOuts = 0;
-var randomNumberY = Math.floor(Math.random() * ((400 - 550) +1) + 550);
+var canvas = document.getElementById("myCanvas");//gets the canvas element that I made on the html
+var ctx = canvas.getContext("2d");//sets the canvas to 2d mode
+var randomNumber = Math.floor(Math.random() * ((-10 - 10)+ 1) + 10);//variable holds a random number between -10 and 10. used below to randomize the direction of hits
+var homeRun = 0;//varialbe to hold the number of homeruns for the player
+var totalOuts = 0;//variable to hold the total number of outs for the player
+var randomNumberY = Math.floor(Math.random() * ((400 - 550) +1) + 550);//variable that holds a random number between 400 and 550. used belwo to randomize the depth of "out" hits.
 
-var newPitch = function (){
+var newPitch = function (){//starts a function that make a new pitch happen after each hit.  used below in the move function
 	
-	ball.position.x = 600;
-  	ball.position.y = 570;
+	ball.position.x = 600;//sets the position of the ball on the x axis to 600
+  	ball.position.y = 570;//sets the postiion of the ball on the y axis to 570
 
-  	ball.direction = "pitch";
+  	ball.direction = "pitch";//changes the value of the direction property in the ball object to pitch
 }
 
-var ball = {
-	position: {x:600, y:570},
-	direction: "pitch",
-	move: function() {
- //move the ball down the Y axis
- 		if (ball.direction === "pitch") {
- 				ball.position.y += 1;
- 		} else if (ball.direction === "HR") {
- 				ball.position.y -= 5;
- 				ball.position.x += randomNumber;
-  		} else if (ball.direction === "out") {
-  				ball.position.y -= 5;
-  				ball.position.x += randomNumber;
-  		} else if (ball.direction === "stop") {
-  				ball.position.y += 0;
-  				ball.position.x -= 0;
+var ball = {//variable ball that holds that is an object with properties for the baseball.
+	position: {x:600, y:570},//holds the x and y coordinates of the ball. starts at x600 and y500
+	direction: "pitch",//denotes what the ball is doing.  different states make the ball do different things
+	move: function() {//start of the funtion that is responsible for the movement of the ball.
+ 		if (ball.direction === "pitch") {//condition statement that checks if the direction of the ball is "pitch".
+ 				ball.position.y += 1;//moves the ball position by 1 pixel down the y axis as long as ball direction is pitch
+ 		} else if (ball.direction === "HR") {//condition statement that checks if the direction of the ball is HR(home run)
+ 				ball.position.y -= 5;//moves the ball position by 5 pixels up the y axis as long as direction is HR
+ 				ball.position.x += randomNumber;//moves the ball by a random number between -10 and 10 across the x axis. this randomizes which side of the field the ball is hit to.
+  		} else if (ball.direction === "out") {//conditional statement to check if the direction of the ball is "out"
+  				ball.position.y -= 5;//moves the position of the ball up the y axis by 5
+  				ball.position.x += randomNumber;//moves the ball by a random number between -10 and 10 across the x axis. this randomizes which side of the field the ball is hit to.
+  		} else if (ball.direction === "stop") {//conditional statement checks to see if the direction of the ball is "stop"
+  				ball.position.y += 0;//makes the ball stop moving on the y axis
+  				ball.position.x -= 0;//makes the ball stop moving on the x axis
   				// console.log(ball.direction);
-  				setTimeout(newPitch, 2000);
-  		} else if (ball.direction === "strike"){
+  				setTimeout(newPitch, 2000);//this activates the function newPitch after 2 seconds.  new pitch starts the next pitch in the game
+  		} else if (ball.direction === "strike"){//conditional statement checks to see if the ball direction is strike
 
-  				setTimeout(newPitch, 2000);
+  				setTimeout(newPitch, 2000);//this activates the function newPitch after 2 seconds.  new pitch starts the next pitch in the game
   			}
 
-  		if (ball.position.y > 646) {
+  		if (ball.position.y > 646) {//checks to see if the position of the ball on the y axis is greater than 646
 
-  				ball.direction = "stop";
-				ball.position.x = 600;
-				ball.position.y = 570;
-				totalOuts = totalOuts += 1;
+  				ball.direction = "stop";//changes the ball direction to stop
+				ball.position.x = 600;//sets the x position of the ball to 600
+				ball.position.y = 570;//sets the y position of the ball to 570
+				totalOuts = totalOuts += 1;//increases the total outs by 1
 				console.log("strike looking",  "out #" + totalOuts);
   		}	
   			// console.log(ball.direction, ball.position.y, randomNumberY);
-  		if (ball.direction === "out" && ball.position.y > (randomNumberY - 5) && ball.position.y < (randomNumberY + 5)) {
+  		if (ball.direction === "out" && ball.position.y > (randomNumberY - 5) && ball.position.y < (randomNumberY + 5)) {//checks if the ball direction is out and if the ball postion y is greater than or less than a radnom number between 400 and 550
   				// console.log("woohoo", ball.position.y);
-  				ball.direction = "stop";	 				
+  				ball.direction = "stop";//changes the ball direction to stop	 				
   		}
-  		if (ball.direction === "HR" && ball.position.y < 0) {
+  		if (ball.direction === "HR" && ball.position.y < 0) {//conditional statement checks to see the ball direction is HR and the y position of the ball is less than 0
   				
-  				ball.direction = "stop";
+  				ball.direction = "stop";//changes the ball direction to stop
   				// console.log(ball.direction, ball.position.y);
   		}
 	},
-	draw: function(){
-		ctx.clearRect(0,0,canvas.width,canvas.height);
-		ctx.beginPath();
-		ctx.fillStyle = 'white';
-		ctx.arc(ball.position.x, ball.position.y, 3, 0, Math.PI*2);
-		ctx.fill();
+	draw: function(){//this function draws the ball
+		ctx.clearRect(0,0,canvas.width,canvas.height);//clears the canvas
+		ctx.beginPath();//gets read to draw
+		ctx.fillStyle = 'white';//makdes the fill fo the ball white
+		ctx.arc(ball.position.x, ball.position.y, 3, 0, Math.PI*2);//creates the circle that is the ball
+		ctx.fill();//fill the circle
 	},
 
 }
 
-var animateCanvas = function(){
+var animateCanvas = function(){//function to animate the canvas
  
-    ball.move();
-  	ball.draw();
+    ball.move();//runs the move function from the ball object
+  	ball.draw();//runs the draw function from the draw object
 
-  	window.requestAnimationFrame(animateCanvas);
+  	window.requestAnimationFrame(animateCanvas);.//runs the animate canvas 60 fps
 }
-animateCanvas();
+animateCanvas();//runs the animate canvas function
 
  
 document.addEventListener("keydown", function(){
